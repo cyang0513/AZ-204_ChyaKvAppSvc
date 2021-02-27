02345b7335df2473d7cbf54e911785bea81c357a
Chengkai Yang <yangchengkai@gmail.com>
2021-02-27 22:42:20 +0100

    swagger as default page

--- ChyaKvAppSvc/Startup.cs
+++ ChyaKvAppSvc/Startup.cs
@@ -43,7 +43,9 @@ namespace ChyaKvAppSvc
       {
          app.UseDeveloperExceptionPage();
          app.UseSwagger();
-         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChyaKvAppSvc v1"));
+         app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChyaKvAppSvc v1");
+            c.RoutePrefix = string.Empty;
+         });
 
          app.UseHttpsRedirection();
 
